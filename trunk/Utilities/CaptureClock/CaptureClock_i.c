111c97015d295aa9ed6a916f13dad6cae454ac2c
roman <roman@9f0a43ce-f1ea-4ba9-b87d-1d20923592e0>
2012-08-26 18:05:22 +0000

    online result submission

--- trunk/Utilities/CaptureClock/CaptureClock_i.c
+++ trunk/Utilities/CaptureClock/CaptureClock_i.c
@@ -6,7 +6,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Sun Aug 26 16:38:39 2012
+/* at Sun Aug 26 17:11:45 2012
  */
 /* Compiler settings for CaptureClock.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
