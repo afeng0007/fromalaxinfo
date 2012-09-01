f2ce32aa31e9415a51ef80da4556bd8908cc998e
roman <roman@9f0a43ce-f1ea-4ba9-b87d-1d20923592e0>
2012-09-01 12:57:35 +0000

    Progress Animation VB.NET UserControl

---
+++ Snippets/ProgressAnimation/MainForm.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Diagnostics
+Imports System.Drawing
+Imports System.Drawing.Imaging
+
+Public Class MainForm
+    Private WithEvents Animation As ProgressAnimation
+
+    Private Sub MainForm_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Animation = New ProgressAnimation
+        Controls.Add(Animation)
+        Animation.Dock = DockStyle.Fill
+        Dim CurrentScreen As Screen = Screen.FromControl(Me)
+        Width = CurrentScreen.WorkingArea.Width * 7 / 8
+        Height = CurrentScreen.WorkingArea.Height * 7 / 8
+        Left = (CurrentScreen.WorkingArea.Width - Width) / 2
+        Top = (CurrentScreen.WorkingArea.Height - Height) / 2
+    End Sub
+    Private Sub MainForm_KeyPress(sender As System.Object, e As System.Windows.Forms.KeyPressEventArgs) Handles MyBase.KeyPress
+        If e.KeyChar = Chr(27) Then Close()
+    End Sub
+    Private Sub Animation_KeyPress(sender As System.Object, e As System.Windows.Forms.KeyPressEventArgs) Handles Animation.KeyPress
+        If e.KeyChar = Chr(27) Then Close()
+    End Sub
+End Class
