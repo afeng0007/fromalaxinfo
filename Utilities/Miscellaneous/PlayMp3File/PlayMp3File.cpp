66743daf2feb9ee7ee8a9bbe720d7ea21da8daa4
roman <roman@9f0a43ce-f1ea-4ba9-b87d-1d20923592e0>
2012-10-16 19:32:20 +0000

    better readability with V macro

--- Utilities/Miscellaneous/PlayMp3File/PlayMp3File.cpp
+++ Utilities/Miscellaneous/PlayMp3File/PlayMp3File.cpp
@@ -11,36 +11,38 @@
 
 #pragma comment(lib, "strmiids.lib")
 
+#define V(x) ATLVERIFY(SUCCEEDED(x))
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	static LPCTSTR g_pszPath = _T("F:\\Music\\Cher - Walking In Memphis.mp3");
-	ATLVERIFY(SUCCEEDED(CoInitialize(NULL)));
+	V(CoInitialize(NULL));
 	{
 		CComPtr<IGraphBuilder> pGraphBuilder;
-		ATLVERIFY(SUCCEEDED(pGraphBuilder.CoCreateInstance(CLSID_FilterGraph)));
+		V(pGraphBuilder.CoCreateInstance(CLSID_FilterGraph));
 		// NOTE: A simpler version is good for the files you are sure of that they don't have too much of ID3 tags on the header:
 		//       with a large headers the files might be rejected; the longer versions takes all .MP3 files
 		#if TRUE
 			CComPtr<IBaseFilter> pBaseFilter;
-			ATLVERIFY(SUCCEEDED(pBaseFilter.CoCreateInstance(CLSID_WMAsfReader)));
+			V(pBaseFilter.CoCreateInstance(CLSID_WMAsfReader));
 			CComQIPtr<IFileSourceFilter> pFileSourceFilter = pBaseFilter;
 			ATLASSERT(pFileSourceFilter);
-			ATLVERIFY(SUCCEEDED(pFileSourceFilter->Load(CT2COLE(g_pszPath), NULL)));
-			ATLVERIFY(SUCCEEDED(pGraphBuilder->AddFilter(pBaseFilter, NULL)));
+			V(pFileSourceFilter->Load(CT2COLE(g_pszPath), NULL));
+			V(pGraphBuilder->AddFilter(pBaseFilter, NULL));
 			CComPtr<IEnumPins> pEnumPins;
-			ATLVERIFY(SUCCEEDED(pBaseFilter->EnumPins(&pEnumPins)));
+			V(pBaseFilter->EnumPins(&pEnumPins));
 			CComPtr<IPin> pPin;
 			ATLVERIFY(pEnumPins->Next(1, &pPin, NULL) == S_OK);
-			ATLVERIFY(SUCCEEDED(pGraphBuilder->Render(pPin)));
+			V(pGraphBuilder->Render(pPin));
 		#else
-			ATLVERIFY(SUCCEEDED(pGraphBuilder->RenderFile(CT2CW(g_pszPath), NULL)));
+			V(pGraphBuilder->RenderFile(CT2CW(g_pszPath), NULL)));
 		#endif
 		CComQIPtr<IMediaControl> pMediaControl = pGraphBuilder;
 		CComQIPtr<IMediaEvent> pMediaEvent = pGraphBuilder;
 		ATLASSERT(pMediaControl && pMediaEvent);
-		ATLVERIFY(SUCCEEDED(pMediaControl->Run()));
+		V(pMediaControl->Run());
 		LONG nEventCode = 0;
-		ATLVERIFY(SUCCEEDED(pMediaEvent->WaitForCompletion(INFINITE, &nEventCode)));
+		V(pMediaEvent->WaitForCompletion(INFINITE, &nEventCode));
 	}
 	CoUninitialize();
 	return 0;
