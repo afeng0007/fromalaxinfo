bb81cee0599803c7a3613a417e3d9778099d4c7a
roman <roman@9f0a43ce-f1ea-4ba9-b87d-1d20923592e0>
2012-11-26 12:00:51 +0000

    Some 500 more identifiers

--- Utilities/ShowHresult/ShowHresult_i.h
+++ Utilities/ShowHresult/ShowHresult_i.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 7.00.0555 */
-/* at Thu Oct 11 23:19:07 2012
+/* at Mon Nov 26 13:58:28 2012
  */
 /* Compiler settings for ShowHresult.idl:
     Oicf, W1, Zp8, env=Win32 (32b run), target_arch=X86 7.00.0555 
