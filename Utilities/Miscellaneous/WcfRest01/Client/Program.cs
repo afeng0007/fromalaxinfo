5e5328da134c3465c68c883ac48cfd4b0b80d4b6
roman <roman@9f0a43ce-f1ea-4ba9-b87d-1d20923592e0>
2013-02-03 15:52:03 +0000

    Non-REST base

---
+++ Utilities/Miscellaneous/WcfRest01/Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.Sleep(1000);
+            ServerServiceReference.ServiceClient client = new ServerServiceReference.ServiceClient();
+            for (int iteration = 0; ; iteration++)
+            {
+                string input = char.ConvertFromUtf32('A' + (iteration / 10)) + (iteration % 10).ToString();
+                string output = client.Method(input);
+                Console.WriteLine(string.Format("Request sent/received, input {0}, output {1}", input, output));
+                Thread.Sleep(2 * 1000);
+            }
+        }
+    }
+}
---
+++ Utilities/Miscellaneous/WcfRest01/Server/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace Server
+{
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        string Method(string input);
+    }
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class Service :
+        IService
+    {
+        #region IService
+        public string Method(string input)
+        {
+            string output = input.Substring(1) + input[0];
+            Console.WriteLine(string.Format("Request received, input {0}, output {1}", input, output));
+            return output;
+        }
+        #endregion
+    }
+
+    class Program
+    {
+        private ServiceHost serviceHost = null;
+
+        private void InternalMain(string[] args)
+        {
+            Debug.Assert(serviceHost == null);
+            Console.WriteLine("Starting...");
+            serviceHost = new ServiceHost(new Service());
+            Console.WriteLine("Started.");
+            try
+            {
+                serviceHost.Open();
+                Console.WriteLine("Running, press any key to close...");
+                Console.ReadKey();
+                Console.WriteLine();
+            }
+            catch (Exception exception)
+            {
+                serviceHost.Close();
+                serviceHost = null;
+                throw exception;
+            }
+            Console.WriteLine("Stopping...");
+            serviceHost.Close();
+            Console.WriteLine("Stopped.");
+        }
+        static void Main(string[] args)
+        {
+            Program instance = new Program();
+            instance.InternalMain(args);
+        }
+    }
+}
