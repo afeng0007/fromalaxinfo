9992f6bff681a765edb092f8f3d4a44db420a38e
roman <roman@9f0a43ce-f1ea-4ba9-b87d-1d20923592e0>
2013-07-08 08:37:24 +0000

    Helper sample - solution and C# project (snippet)

---
+++ DirectShowSpy/Sample/FilterGraphText/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DirectShowLib;
+using AlaxInfoDirectShowSpy;
+
+namespace FilterGraphText
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            IFilterGraph2 graph = new FilterGraph() as IFilterGraph2;
+            graph.RenderFile(@"E:\Media\GoPro 2010 Highlights - You in HD - 1920x1080.mp4", "");
+            FilterGraphHelper helper = new FilterGraphHelper();
+            helper.FilterGraph = graph;
+            Console.WriteLine(helper.Text);
+        }
+    }
+}
