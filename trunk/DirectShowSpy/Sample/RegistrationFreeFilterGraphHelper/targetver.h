64524b3c4c55cd72405ff9f595d8c00b221a18b4
roman <roman@9f0a43ce-f1ea-4ba9-b87d-1d20923592e0>
2014-01-10 11:56:30 +0000

    Cosmetic updates; added registration free C++ sample for filter graph helper

---
+++ trunk/DirectShowSpy/Sample/RegistrationFreeFilterGraphHelper/targetver.h
@@ -0,0 +1,11 @@
+////////////////////////////////////////////////////////////
+// Copyright (C) Roman Ryltsov, 2008-2014
+// Created by Roman Ryltsov roman@alax.info
+
+#pragma once
+
+// Including SDKDDKVer.h defines the highest available Windows platform.
+// If you wish to build your application for a previous Windows platform, include WinSDKVer.h and
+// set the _WIN32_WINNT macro to the platform you wish to support before including SDKDDKVer.h.
+
+#include <SDKDDKVer.h>
