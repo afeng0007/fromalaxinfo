d88089e92dfe405c0565e2dd63d080cc9766cebb
roman <roman@9f0a43ce-f1ea-4ba9-b87d-1d20923592e0>
2014-01-10 10:12:54 +0000

    Added FilterGraphHelperDialog C# sample

---
+++ DirectShowSpy/Sample/FilterGraphHelperDialog/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DirectShowLib;
+using AlaxInfoDirectShowSpy;
+
+namespace FilterGraphHelperDialog
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            IFilterGraph2 graph = new FilterGraph() as IFilterGraph2;
+            graph.RenderFile(@"E:\Media\GoPro 2010 Highlights - You in HD - 1920x1080.mp4", "");
+            FilterGraphHelper helper = new FilterGraphHelper();
+            helper.FilterGraph = graph;
+            helper.DoPropertyFrameModal(0);
+            Console.WriteLine(helper.Text);
+        }
+    }
+}
