0830f14272e4391b5b7d5504149b203a2a396b23
roman <roman@9f0a43ce-f1ea-4ba9-b87d-1d20923592e0>
2016-01-20 10:48:16 +0000

    Upgraded to VS 2013 and checked with current Zeranoe FFmpeg

--- trunk/Utilities/FFmpeg/FFmpegApp/FFmpegApp.cpp
+++ trunk/Utilities/FFmpeg/FFmpegApp/FFmpegApp.cpp
@@ -1,18 +1,19 @@
 ////////////////////////////////////////////////////////////
-// Copyright (C) Roman Ryltsov, 2008-2012
+// Copyright (C) Roman Ryltsov, 2008-2016
 // Created by Roman Ryltsov roman@alax.info
-// 
-// $Id$
 
 #include "stdafx.h"
 
 extern "C" 
 {
-	#include "dev\include\libavcodec\avcodec.h"
+	// NOTE: Additional directory ..\zeranoe.com\dev\include gets to the files
+	#include "libavcodec\avcodec.h"
 }
 
-#pragma comment(lib, "dev\\lib\\avcodec.lib")
+// NOTE: Additional directory ..\zeranoe.com\dev\lib gets to the files
+#pragma comment(lib, "avcodec.lib")
 
+// NOTE: Be sure to copy DLL files from ..\zeranoe.com\shared\bin to the directory of the FFmpegApp.exe binary
 int _tmain(int argc, _TCHAR* argv[])
 {
 	_tprintf(_T("Trying avcodec_register_all... "));
