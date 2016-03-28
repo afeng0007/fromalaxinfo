b07324114d5b16cd1f61b7ef352f60a7f678200d
roman <roman@9f0a43ce-f1ea-4ba9-b87d-1d20923592e0>
2016-03-28 18:30:35 +0000

    Code formatting

--- Utilities/Miscellaneous/ComEvents01/Consumer/targetver.h
+++ Utilities/Miscellaneous/ComEvents01/Consumer/targetver.h
@@ -1,3 +1,10 @@
+////////////////////////////////////////////////////////////
+// Copyright (C) Roman Ryltsov, 2016
+// Created by Roman Ryltsov roman@alax.info
+// 
+// A permission to use the source code is granted as long as reference to 
+// source website http://alax.info is retained.
+
 #pragma once
 
 // Including SDKDDKVer.h defines the highest available Windows platform.
