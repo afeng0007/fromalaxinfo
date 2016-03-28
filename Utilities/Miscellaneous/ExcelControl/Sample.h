cff78dd52cae6b8f51fbac2df0977010f131141c
roman <roman@9f0a43ce-f1ea-4ba9-b87d-1d20923592e0>
2016-03-28 18:27:28 +0000

    Code formatting

--- Utilities/Miscellaneous/ExcelControl/Sample.h
+++ Utilities/Miscellaneous/ExcelControl/Sample.h
@@ -1,16 +1,20 @@
-// Sample.h : Declaration of the CSample
+////////////////////////////////////////////////////////////
+// Copyright (C) Roman Ryltsov, 2016
+// Created by Roman Ryltsov roman@alax.info
+// 
+// A permission to use the source code is granted as long as reference to 
+// source website http://alax.info is retained.
+
 #pragma once
 #include "resource.h"       // main symbols
 #include <atlctl.h>
 #include "ExcelControl_i.h"
 
 #if defined(_WIN32_WCE) && !defined(_CE_DCOM) && !defined(_CE_ALLOW_SINGLE_THREADED_OBJECTS_IN_MTA)
-#error "Single-threaded COM objects are not properly supported on Windows CE platform, such as the Windows Mobile platforms that do not include full DCOM support. Define _CE_ALLOW_SINGLE_THREADED_OBJECTS_IN_MTA to force ATL to support creating single-thread COM object's and allow use of it's single-threaded COM object implementations. The threading model in your rgs file was set to 'Free' as that is the only threading model supported in non DCOM Windows CE platforms."
+	#error "Single-threaded COM objects are not properly supported on Windows CE platform, such as the Windows Mobile platforms that do not include full DCOM support. Define _CE_ALLOW_SINGLE_THREADED_OBJECTS_IN_MTA to force ATL to support creating single-thread COM object's and allow use of it's single-threaded COM object implementations. The threading model in your rgs file was set to 'Free' as that is the only threading model supported in non DCOM Windows CE platforms."
 #endif
 
 using namespace ATL;
-
-
 
 // CSample
 class ATL_NO_VTABLE CSample :
--- Utilities/Miscellaneous/ExcelControl/dllmain.h
+++ Utilities/Miscellaneous/ExcelControl/dllmain.h
@@ -1,4 +1,9 @@
-// dllmain.h : Declaration of module class.
+////////////////////////////////////////////////////////////
+// Copyright (C) Roman Ryltsov, 2016
+// Created by Roman Ryltsov roman@alax.info
+// 
+// A permission to use the source code is granted as long as reference to 
+// source website http://alax.info is retained.
 
 class CExcelControlModule : public ATL::CAtlDllModuleT< CExcelControlModule >
 {
