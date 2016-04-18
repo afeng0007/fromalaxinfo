0150a0e0426da9d2935f886de74fa689417aa727
roman <roman@9f0a43ce-f1ea-4ba9-b87d-1d20923592e0>
2016-04-18 11:03:32 +0000

    MediaSampleTrace UI small improvements - ability to switch back to live data from context menu; SystemDeviceEnumeratorSpy is extended to track IMMDeviceActivator interface and blind-forward other interfaces

--- trunk/DirectShowSpy/targetver.h
+++ trunk/DirectShowSpy/targetver.h
@@ -15,8 +15,4 @@
 
 #pragma once
 
-#define WINVER			0x0600        
-#define _WIN32_WINNT	0x0600  
-#define _WIN32_WINDOWS	0x0410
-#define _WIN32_IE		0x0700     
-#define _RICHEDIT_VER	0x0200	// RichEdit 2.0
+#include <sdkddkver.h>
